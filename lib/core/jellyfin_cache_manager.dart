dd6db2815d5421af6d70b73e566fb9c71c6f778f
Rikkichy <vanish-relay@pm.me>
2026-01-16 23:05:49 +0300

    Fix type error

--- lib/core/jellyfin_cache_manager.dart
+++ lib/core/jellyfin_cache_manager.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
-
+import 'package:http/io_client.dart';
 class JellyfinCacheManager extends CacheManager {
   static const key = 'jellyfinCache';
 
@@ -11,8 +11,10 @@ class JellyfinCacheManager extends CacheManager {
     Config(
       key,
       fileService: HttpFileService(
-        httpClient: HttpClient()
-          ..badCertificateCallback = (X509Certificate cert, String host, int port) => true,
+        httpClient: IOClient(
+          HttpClient()
+            ..badCertificateCallback = (X509Certificate cert, String host, int port) => true,
+        ),
       ),
     ),
   );
