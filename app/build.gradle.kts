11739b9011c7b3dabc0f41e8bda7c805953caedc
rii <vanish-relay@pm.me>
2026-03-03 22:05:43 +0300

    Raise app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.muufin.compose"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3.0"
+        versionCode = 5
+        versionName = "0.3.1"
 
         vectorDrawables {
             useSupportLibrary = true
